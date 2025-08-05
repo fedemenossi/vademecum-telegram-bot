8aaf86f9d0a91575a81fe370ebd178c2ebe873b7
Federico Menossi <fmenossi@gmail.com>
2025-08-04 22:59:35 -0300

    g

--- vademecum.py
+++ vademecum.py
@@ -192,7 +192,7 @@ def activar_suscripcion(telegram_id):
 def preguntar_a_chatgpt(mensaje_usuario):
     try:
         response = client.chat.completions.create(
-            model="gpt-g-688561e583788191a145fcdb69e0daa8",
+            model="g-688561e583788191a145fcdb69e0daa8",
             messages=[
                 {"role": "user", "content": mensaje_usuario}
             ]
